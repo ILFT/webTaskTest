4ec6a7b5d1eb98ccc060ec8bfd5e487fe3f230b8
ILFT <ftilmtlk@gmail.com>
2023-03-09 22:18:35 +0300

    0.0.3

--- WebApplication2/Models/Db/DbTasks.cs
+++ WebApplication2/Models/Db/DbTasks.cs
@@ -13,23 +13,44 @@ namespace WebApplication2.Models.Db
         }
         public async Task AddTask(TaskViewModel task) {
             var connection = (new DbContext()).connection;
-            //string commandText = $"WITH NEW_TASK AS (INSERT INTO TASKS (name, dataCreate, dataFinish, deadline, category, priority, comment) values (@nameTask, current_date, NULL, current_date + @duration, (SELECT id FROM CATEGORY WHERE name = @nameCategories), (SELECT id FROM PRIORITY WHERE name = @namePriority), @commentTask ) returning id) INSERT INTO task_tag SELECT (SELECT id FROM NEW_TASK), tag.id FROM  tag  WHERE tag.name in (@tagArray)";
-            string commandText = $"WITH NEW_TASK AS (\r\nINSERT INTO TASKS (name, dataCreate, dataFinish, deadline, category, priority, comment) \r\nvalues \r\n('Тестовый проект csharp', current_date, NULL, current_date + 7, (SELECT id FROM CATEGORY WHERE name = 'Поручение'), (SELECT id FROM PRIORITY WHERE name = 'высокий'), 'проект для проверки работоспособности' )\r\nreturning id\r\n)\r\nINSERT INTO task_tag\r\nSELECT (SELECT id FROM NEW_TASK), tag.id\r\nFROM  tag \r\nWHERE tag.name in ('Аренды','Лицензия')\r\n";
+            string commandText = $"WITH NEW_TASK AS (INSERT INTO TASKS (name, dataCreate, dataFinish, deadline, category, priority, comment) values (@nameTask, current_date, NULL, current_date + @duration, (SELECT id FROM CATEGORY WHERE name = @nameCategories), (SELECT id FROM PRIORITY WHERE name = @namePriority), @commentTask ) returning id) INSERT INTO task_tag SELECT (SELECT id FROM NEW_TASK), tag.id FROM  tag  WHERE tag.name in (@tagArray)";
+            //string commandText = $"WITH NEW_TASK AS (\r\nINSERT INTO TASKS (name, dataCreate, dataFinish, deadline, category, priority, comment) \r\nvalues \r\n('Тестовый проект csharp', current_date, NULL, current_date + 7, (SELECT id FROM CATEGORY WHERE name = 'Поручение'), (SELECT id FROM PRIORITY WHERE name = 'высокий'), 'проект для проверки работоспособности' )\r\nreturning id\r\n)\r\nINSERT INTO task_tag\r\nSELECT (SELECT id FROM NEW_TASK), tag.id\r\nFROM  tag \r\nWHERE tag.name in ('Аренды','Лицензия')\r\n";
 
 
-            await using (var cmd = new NpgsqlCommand(commandText, connection))
+            /*await using var cmd = new NpgsqlCommand(commandText, connection)
             {
-                //cmd.Parameters.AddWithValue("nameTask", task.Name);
-                //cmd.Parameters.AddWithValue("duration", (task.Deadline - task.DateCreate).Days);
-                //cmd.Parameters.AddWithValue("nameCategories", task.Category);
-                //cmd.Parameters.AddWithValue("namePriority", task.Priority);
-                //cmd.Parameters.AddWithValue("commentTask", task.Comment);
-                //cmd.Parameters.AddWithValue("tagArray", task.Tags);
+                cmd.Parameters.AddWithValue("nameTask", task.Name);
+                cmd.Parameters.AddWithValue("duration", (task.Deadline - task.DateCreate).Days);
+                cmd.Parameters.AddWithValue("nameCategories", task.Category);
+                cmd.Parameters.AddWithValue("namePriority", task.Priority);
+                cmd.Parameters.AddWithValue("commentTask", task.Comment);
+                cmd.Parameters.AddWithValue("tagArray", task.Tags);
 
                 Console.WriteLine(cmd.CommandText);
-                int rows = await cmd.ExecuteNonQueryAsync();
-                
-            }
+
+            }*/
+
+            await using var cmd = new NpgsqlCommand(commandText, connection)
+            {
+                Parameters =
+                {
+                    new("nameTask", task.Name),
+                    new("duration", (task.Deadline - task.DateCreate).Days),
+                    new("nameCategories", task.Category),
+                    new("namePriority", task.Priority),
+                    new("commentTask", task.Comment),
+                    new("tagArray", String.Concat(task.Tags)),
+                }
+            };
+            Console.WriteLine(cmd.Parameters[0].Value);
+            Console.WriteLine(cmd.Parameters[1].Value);
+            Console.WriteLine(cmd.Parameters[2].Value);
+            Console.WriteLine(cmd.Parameters[3].Value);
+            Console.WriteLine(cmd.Parameters[4].Value);
+            Console.WriteLine(String.Concat(cmd.Parameters[5].Value));
+
+            int rows = await cmd.ExecuteNonQueryAsync();
+
         }
     }
 }
